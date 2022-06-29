4f20f19121f5eeb4be7433087d7ae13baafbe6cc
Pawelele21 <zalewskipawel603@gmail.com>
2022-06-29 19:31:27 +0200

    Initial commit

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/HelloApplication.java
@@ -0,0 +1,27 @@
+package pl.lublin.wsei.java.cwiczenia.lab3;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gusInfoGraphic.fxml"));
+        Parent root = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        controller.setHostServices(this.getHostServices());
+        controller.setStage(stage);
+
+        stage.setTitle("Infografiki");
+        stage.setScene(new Scene(root, 800, 700));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/Infografika.java
@@ -0,0 +1,61 @@
+package pl.lublin.wsei.java.cwiczenia.lab3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Infografika {
+
+    public String tytul;
+    public String adresStrony;
+    public String adresMiniaturki;
+    public String adresGrafiki;
+    public int szerokosc;
+    public int wysokosc;
+
+    public Infografika(String tekst) {
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(tekst);
+        if(m.find()) {
+            tytul = m.group(1);
+        } else {
+            tytul = "";
+        }
+
+        pat = Pattern.compile("<link>(.*)</link>");
+        m = pat.matcher(tekst);
+        if(m.find()) {
+            adresStrony = m.group(1);
+        } else {
+            adresStrony = "";
+        }
+
+        pat = Pattern.compile("<media:thumbnail url=\"([^\"]*)\"");
+        m = pat.matcher(tekst);
+        if(m.find()) {
+            adresMiniaturki = m.group(1);
+        } else {
+            adresMiniaturki = "";
+        }
+
+        pat = Pattern.compile("<media:content url=\"([^\"]*)\"(.*)width=\"([^\"]*)\".height=\"([^\"]*)");
+        m = pat.matcher(tekst);
+        if(m.find()) {
+            adresGrafiki = m.group(1);
+            szerokosc = Integer.parseInt(m.group(3));
+            wysokosc = Integer.parseInt(m.group(4));
+        } else {
+            adresGrafiki = "";
+            szerokosc = 0;
+            wysokosc = 0;
+        }
+    }
+
+    public void print() {
+        System.out.println("Infografika:");
+        System.out.println("\ttytul: " + tytul);
+        System.out.println("\tadres strony: " + adresStrony);
+        System.out.println("\tadres grafiki: " + adresGrafiki);
+        System.out.println("\tadres miniaturki: " + adresMiniaturki);
+        System.out.println("\trozmiary: " + szerokosc + "x" + wysokosc + "\n");
+    }
+}
